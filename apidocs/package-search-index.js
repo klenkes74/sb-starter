12b1a0461407e9175cca84b783b73a221608e6ab
Roland T. Lichti <rlichti@kaiserpfalz-edv.de>
2024-02-11 06:06:33 +0100

    Site checkin for project Spring Boot Starter

---
+++ apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"Alle Packages","u":"allpackages-index.html"},{"l":"de.lichti.klenkes74.starter"},{"l":"de.lichti.klenkes74.starter.controller"},{"l":"de.lichti.klenkes74.starter.errors"},{"l":"de.lichti.klenkes74.starter.logging"},{"l":"de.lichti.klenkes74.starter.openapi"}];updateSearchResults();
